3ed42917e8e777d64f19704c3a16cae094239a84
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-04-20 14:51:53 -0500

    added xsd validation support

--- lib/opinionated-xml/ox.rb
+++ lib/opinionated-xml/ox.rb
@@ -1,19 +1,58 @@
+require "open-uri"
+
 module OX
   
   # Class Methods -- These methods will be available on classes that include OX (ie. the OX MODS class)
   
   module ClassMethods
     
-    attr_accessor :root_property_ref, :root_config, :ox_namespaces
+    attr_accessor :root_property_ref, :root_config, :ox_namespaces, :schema_url
+    attr_writer :schema_file
     
     def root_property( property_ref, path, namespace, opts={})
       @root_config = opts.merge({:namespace=>namespace, :path=>path, :ref=>property_ref})
       @root_property_ref = property_ref
       @ox_namespaces = {'oxns'=>@root_config[:namespace]}
+      @schema_url = opts[:schema]
     end
     
     def property( property_ref, path, &block)
     end
+    
+    def validate(doc)
+      schema.validate(doc).each do |error|
+          puts error.message
+      end
+    end
+    
+    def schema
+      @schema ||= Nokogiri::XML::Schema(schema_file.read)
+    end
+    
+    def schema_file
+      @schema_file ||= file_from_url(schema_url)
+    end
+    
+    def file_from_url( url )
+      # parsed_url = URI.parse( url )
+      # 
+      # if parsed_url.class != URI::HTTP 
+      #   raise "Invalid URL.  Could not parse #{url} as a HTTP url."
+      # end
+      
+      begin
+        file = open( url )
+        return file
+      rescue OpenURI::HTTPError => e
+        raise "Could not retrieve file from #{url}. Error: #{e}"
+      rescue Exception => e  
+        raise "Could not retrieve file from #{url}. Error: #{e}"
+      end
+    end
+    
+    private :file_from_url
+    
+    
     
   end
   
@@ -28,6 +67,10 @@ module OX
   # Most importantly, this matches the 'oxns' namespace to the namespace you provided in your root property config
   def ox_namespaces
     @ox_namespaces ||= namespaces.merge(self.class.ox_namespaces)
+  end
+  
+  def validate
+    self.class.validate(self)
   end
 
 end
--- spec/unit/opinionated-xml_spec.rb
+++ spec/unit/opinionated-xml_spec.rb
@@ -16,7 +16,7 @@ describe "OpinionatedXml" do
       #           :attributes=>[],
       #           :subelements => :mods
                      
-      root_property :mods, "mods", "http://www.loc.gov/mods/v3", :attributes=>["id", "version"]          
+      root_property :mods, "mods", "http://www.loc.gov/mods/v3", :attributes=>["id", "version"], :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd"          
                 
                 
       property :name_, :path=>"name", 
@@ -32,8 +32,10 @@ describe "OpinionatedXml" do
                   
       property :person, :variant_of=>:name_, :attributes=>{:type=>"personal"}
       
-      property :role, :path=>[:name_, "role/roleTerm"],
-                  :attributes=>[ { "type"=>["text", "code"] } , "authority"]
+      property :role, :path=>[:name_, "role"],
+                  :attributes=>[ { "type"=>["text", "code"] } , "authority"],
+                  :default_content_path => "roleTerm"
+                  
                   
     end
     
@@ -45,6 +47,7 @@ describe "OpinionatedXml" do
       root_property :other, "other", "http://www.foo.com"        
       
     end
+        
   end
   
   before(:each) do
@@ -55,62 +58,120 @@ describe "OpinionatedXml" do
     Object.send(:remove_const, :FakeOxMods)
   end
   
-  describe "root_property" do
+  describe "#new" do
+    it "should set up namespaces" do
+      @fakemods.ox_namespaces.should == {"oxns"=>"http://www.loc.gov/mods/v3", "xmlns:ns2"=>"http://www.w3.org/1999/xlink", "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance", "xmlns:ns3"=>"http://www.loc.gov/mods/v3"}
+    end
+  end
+  
+  describe "#root_property" do
     it "should initialize root_property class attributes without attributes bleeding over to other OX classes" do
       FakeOxMods.root_property_ref.should == :mods
-      FakeOxMods.root_config.should == {:ref=>:mods, :path=>"mods", :namespace=>"http://www.loc.gov/mods/v3", :attributes=>["id", "version"]}
+      FakeOxMods.root_config.should == {:ref=>:mods, :path=>"mods", :namespace=>"http://www.loc.gov/mods/v3", :attributes=>["id", "version"], :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd"}
       FakeOxMods.ox_namespaces.should == {"oxns"=>"http://www.loc.gov/mods/v3"}
+      FakeOxMods.schema_url.should == "http://www.loc.gov/standards/mods/v3/mods-3-2.xsd"
       
       FakeOtherOx.root_property_ref.should == :other
       FakeOtherOx.root_config.should == {:namespace=>"http://www.foo.com", :path=>"other", :ref=>:other}
     end
   end
   
-  describe "property" do
+  describe "#property" do
+  
+    it "fails gracefully if you try to look up nodes for an undefined property" do
+      @fakemods.lookup(:nobody_home).should == []
+    end
+  
+    it "constructs xpath queries for you" do
+    
+      @fakemods.expects(:xpath).with('//oxns:name[@type="person"]', @fakemods.ox_namespaces)
+      @fakemods.lookup(:person)
+    
+      @fakemods.expects(:xpath).with('//oxns:name[@type="person" and contains(oxns:namePart[@type="date"], "2010") ]', @fakemods.ox_namespaces)
+      @fakemods.lookup(:person, :date=>"2010")
+    
+      @fakemods.expects(:xpath).with('//oxns:name[contains(oxns:role/oxns:roleterm, "donor") and @type="person"]', @fakemods.ox_namespaces)
+      @fakemods.lookup(:person, :role=>"donor")
+    
+    end
+  
   end
   
-  describe "new" do
-    it "should be great" do
-      @fakemods.ox_namespaces.should == {"oxns"=>"http://www.loc.gov/mods/v3", "xmlns:ns2"=>"http://www.w3.org/1999/xlink", "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance", "xmlns:ns3"=>"http://www.loc.gov/mods/v3"}
+  describe ".lookup"  do
+    it "mixes convenience methods into xml nodeSets" do
+      people_set = @fakemods.lookup(:person)
+      people_set.class.should == Nokogiri::XML::NodeSet
+      person1 = people_set.first
+      [:date, :family_name, :given_name, :terms_of_address].each {|cmeth| person1.should respond_to(cmeth)}
+    
+      person1.expects(:xpath).with('namePart[@type="date"]')
+      date_set = person1.date
+      date_set.class.should == Nokogiri::XML::NodeSet
+      date_set.first.class.should == Nokogiri::XML::Element
+
+    end
+  
+    # This lets you benefit from the handy xpath helpers 
+    # without the performance cost of mixing in property-specific convenience methods that you might not be using.
+    it "skips mixing in convenience methods if you tell it not to" do
+      people_set = @fakemods.lookup(:person, :convenience_methods => false)
+      person1 = people_set.first
+      [:date, :family_name, :given_name, :terms_of_address].each {|cmeth| person1.should_not respond_to(cmeth)}
     end
   end
   
-  it "fails gracefully if you try to look up nodes for an undefined property" do
-    @fakemods.lookup(:nobody_home).should == []
+  ## Validation Support 
+  # Some of these tests fail when you don't have an internet connection because the mods schema includes other xsd schemas by URL reference.
+  
+  describe "#schema" do
+    it "should return an instance of Nokogiri::XML::Schema loaded from the schema url -- fails if no internet connection" do
+      FakeOxMods.schema.should be_kind_of Nokogiri::XML::Schema
+    end
   end
   
-  it "constructs xpath queries for you" do
-    
-    @fakemods.expects(:xpath).with('//oxns:name[@type="person"]', @fakemods.ox_namespaces)
-    @fakemods.lookup(:person)
-    
-    @fakemods.expects(:xpath).with('//oxns:name[@type="person" and contains(oxns:namePart[@type="date"], "2010") ]', @fakemods.ox_namespaces)
-    @fakemods.lookup(:person, :date=>"2010")
-    
-    @fakemods.expects(:xpath).with('//oxns:name[contains(oxns:role/oxns:roleterm, "donor") and @type="person"]', @fakemods.ox_namespaces)
-    @fakemods.lookup(:person, :role=>"donor")
-    
+  describe "#validate" do
+    it "should validate the provided document against the schema provided in class definition  -- fails if no internet connection" do
+      FakeOxMods.schema.expects(:validate).with(@fakemods).returns([])
+      FakeOxMods.validate(@fakemods)
+    end
   end
   
-  it "mixes convenience methods into xml nodeSets" do
-    people_set = @fakemods.lookup(:person)
-    people_set.class.should == Nokogiri::XML::NodeSet
-    person1 = people_set.first
-    [:date, :family_name, :given_name, :terms_of_address].each {|cmeth| person1.should respond_to(cmeth)}
+  describe ".validate" do
+    it "should rely on class validate method" do
+      FakeOxMods.expects(:validate).with(@fakemods)
+      @fakemods.validate
+    end
+  end
+  
+  describe "#schema_file" do
+    before(:all) do
+      FakeOxMods.schema_file = nil
+    end
     
-    person1.expects(:xpath).with('namePart[@type="date"]')
-    date_set = person1.date
-    date_set.class.should == Nokogiri::XML::NodeSet
-    date_set.first.class.should == Nokogiri::XML::Element
-
+    after(:all) do
+      FakeOxMods.schema_file = fixture("mods-3-2.xsd")
+    end
+    
+    it "should lazy load the schema file from the @schema_url" do
+      FakeOxMods.instance_variable_get(:@schema_file).should be_nil
+      FakeOxMods.expects(:file_from_url).with(FakeOxMods.schema_url).returns("fake file").once
+      FakeOxMods.schema_file
+      FakeOxMods.instance_variable_get(:@schema_file).should == "fake file"
+      FakeOxMods.schema_file.should == "fake file"
+    end
   end
   
-  # This lets you benefit from the handy xpath helpers 
-  # without the performance cost of mixing in property-specific convenience methods that you might not be using.
-  it "skips mixing in convenience methods if you tell it not to" do
-    people_set = @fakemods.lookup(:person, :convenience_methods => false)
-    person1 = people_set.first
-    [:date, :family_name, :given_name, :terms_of_address].each {|cmeth| person1.should_not respond_to(cmeth)}
+  describe "#file_from_url" do
+    it "should retrieve a file from the provided url over HTTP" do
+      FakeOxMods.send(:file_from_url, "http://google.com")
+    end
+    it "should raise an error if the url is invalid" do
+      lambda {FakeOxMods.send(:file_from_url, "")}.should raise_error(RuntimeError, "Could not retrieve file from . Error: No such file or directory - ")
+      lambda {FakeOxMods.send(:file_from_url, "foo")}.should raise_error(RuntimeError, "Could not retrieve file from foo. Error: No such file or directory - foo")
+    end
+    it "should raise an error if file retrieval fails" do
+      lambda {FakeOxMods.send(:file_from_url, "http://fedora-commons.org/nonexistent_file")}.should raise_error(RuntimeError, "Could not retrieve file from http://fedora-commons.org/nonexistent_file. Error: 404 Not Found")  
+    end
   end
-  
+   
 end
