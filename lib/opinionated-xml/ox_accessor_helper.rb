f0d921d69c65354e9fc375d3f30c23132e43c5df
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-06-19 20:31:34 -0500

    first pass at accessor helper

--- lib/opinionated-xml/ox_accessor_helper.rb
+++ lib/opinionated-xml/ox_accessor_helper.rb
@@ -38,6 +38,79 @@ module OX::AccessorHelper
         
     end
     
+    # Returns the configuration info for the selected accessor.
+    # Ingores any integers in the array (ie. nodeset indices intended for use in other accessor convenience methods)
+    def accessor_info(*args)
+      # Ignore any nodeset indexes in the args array
+      keys = args.select {|x| !x.kind_of?(Integer) }
+      info = @accessors
+      keys.each do |k|
+        unless keys.index(k) == 0
+          info = info.fetch(:children, nil)
+          if info.nil?
+            debugger
+            return nil
+          end
+        end
+        
+        info = info.fetch(k, nil)
+        if info.nil?
+          return nil
+        end
+      end
+      return info
+    end
+    
+    
+    def accessor_xpath(*args)
+      keys = even_values(args)
+      indices = odd_values(args)
+      xpath = "//"
+      keys.each do |k|
+        key_index = keys.index(k)
+        accessor_info = accessor_info(*keys[0..key_index])
+        relative_path = accessor_info[:relative_xpath]
+        
+        if relative_path.kind_of?(Hash)
+          if relative_path.has_key?(:attribute)
+            relative_path = "@"+relative_path[:attribute]
+          end
+        else
+        
+          if indices[key_index]
+            add_position_predicate!(relative_path, indices[key_index])
+          end
+        
+          if accessor_info.has_key?(:default_content_path)
+            relative_path << "/"+accessor_info[:default_content_path]
+          end
+        
+        end
+        if key_index > 0
+          relative_path.insert(0, "/")
+        end
+        xpath << relative_path 
+      end
+      
+      return xpath
+    end
+    
+    def add_position_predicate!(xpath_query, array_index_value)
+      position_function = "position()=#{array_index_value + 1}"
+      
+      if xpath_query.include?("]")
+        xpath_query.insert(xpath_query.rindex("]"), " and #{position_function}")
+      else
+        xpath_query << "[#{position_function}]"
+      end
+    end
+    
+    def odd_values(array)
+      array.values_at(* array.each_index.select {|i| i.odd?})
+    end
+    def even_values(array)
+      array.values_at(* array.each_index.select {|i| i.even?})
+    end
   end
   
   # Instance Methods -- These methods will be available on instances of OX classes (ie. the actual xml documents)
@@ -51,6 +124,8 @@ module OX::AccessorHelper
   # example: [:person, 1, :first_name]
   # Currently, indexes must be integers.
   def retrieve(*args)
+    xpath = self.class.accessor_xpath(*args)    
+    ng_xml.xpath(xpath, "oxns"=>"http://www.loc.gov/mods/v3")    
   end
   
 end
--- spec/unit/accessor_helper_spec.rb
+++ spec/unit/accessor_helper_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "nokogiri"
 
 describe "OX::AccesorHelper" do
   
@@ -10,22 +11,33 @@ describe "OX::AccesorHelper" do
       include OX::AccessorHelper
       #accessor :title, :relative_xpath=>[:titleInfo, :title]}}         
     
-      accessor :title, :relative_xpath=>'oxns:titleInfo/oxns:title', :children=>[
-        {:language =>{:relative_xpath=>[{:attribute=>"lang"}] }}
+      accessor :title_info, :relative_xpath=>'oxns:titleInfo', :children=>[
+        {:main_title=>{:relative_xpath=>'oxns:title'}},         
+        {:language =>{:relative_xpath=>{:attribute=>"lang"} }}
         ]  # this allows you to access the language attribute as if it was a regular child accessor
       accessor :abstract
       accessor :topic_tag, :relative_xpath=>'oxns:subject/oxns:topic'
-      accessor :person, :children=>[
-        {:last_name=>{:relative_xpath=>'oxns:namePart[@type="given"]'}}, 
-        {:first_name=>{:relative_xpath=>'oxns:namePart[@type="fanily"]'}}, 
+      accessor :person, :relative_xpath=>'oxns:name[@type="personal"]',  :children=>[
+        {:last_name=>{:relative_xpath=>'oxns:namePart[@type="family"]'}}, 
+        {:first_name=>{:relative_xpath=>'oxns:namePart[@type="given"]'}}, 
         {:institution=>{:relative_xpath=>'oxns:affiliation'}}, 
         {:role=>{:children=>[
           {:text=>{:relative_xpath=>'oxns:roleTerm[@type="text"]'}},
           {:code=>{:relative_xpath=>'oxns:roleTerm[@type="code"]'}}
         ]}}
       ]
-      accessor :organization, :children=>[:role] 
-      accessor :conference, :children=>[:role]
+      accessor :organization, :relative_xpath=>'oxns:name[@type="institutional"]', :children=>[
+        {:role=>{:children=>[
+          {:text=>{:relative_xpath=>'oxns:roleTerm[@type="text"]'}},
+          {:code=>{:relative_xpath=>'oxns:roleTerm[@type="code"]'}}
+        ]}}
+      ]
+      accessor :conference, :relative_xpath=>'oxns:name[@type="conference"]', :children=>[
+        {:role=>{:children=>[
+          {:text=>{:relative_xpath=>'oxns:roleTerm[@type="text"]'}},
+          {:code=>{:relative_xpath=>'oxns:roleTerm[@type="code"]'}}
+        ]}}
+      ]
       accessor :journal, :relative_xpath=>'oxns:relatedItem[@type="host"]', :children=>[
         # allows for children that are hashes...
         # this allows for more robust handling of nested values (in generating views and when generating solr field names)
@@ -47,7 +59,8 @@ describe "OX::AccesorHelper" do
 
   before(:each) do
     @sample = AccessorTest.new
-    @sample.ng_xml = fixture( File.join("mods_articles", "hydrangea_article1.xml") )
+    article_xml = fixture( File.join("mods_articles", "hydrangea_article1.xml") )
+    @sample.ng_xml = Nokogiri::XML::Document.parse(article_xml)
   end
   
   describe '#accessor' do
@@ -58,34 +71,23 @@ describe "OX::AccesorHelper" do
       AccessorTest.accessors[:journal][:children][:issue][:children][:end_page][:relative_xpath].should == 'oxns:extent[@unit="pages"]/oxns:end'
 
       AccessorTest.accessors[:person][:children][:role][:children][:text][:relative_xpath].should == 'oxns:roleTerm[@type="text"]'
-
     end
   end
   
   describe ".retrieve" do
     it "should use Nokogiri to retrieve a NodeSet corresponding to the combination of accessor keys and array/nodeset indexes" do
-      @sample.retrieve( [:person] ).length.should == 2
+      @sample.retrieve( :person ).length.should == 2
       
-      @sample.retrieve( [:person, 1] ).first.should == @sample.ng_xml.xpath_at('oxns:name[@type="person" and positon()=2]')
-      @sample.retrieve( [:person, 1, :first_name] ).class.should == Nokogiri::NodeSet
-      @sample.retrieve( [:person, 1, :first_name] ).first.text.should == "Siddartha"
+      @sample.retrieve( :person, 1 ).first.should == @sample.ng_xml.xpath('//oxns:name[@type="personal" and position()=2]', "oxns"=>"http://www.loc.gov/mods/v3").first
+      @sample.retrieve( :person, 1, :first_name ).class.should == Nokogiri::XML::NodeSet
+      @sample.retrieve( :person, 1, :first_name ).first.text.should == "Siddartha"
     end
     
     it "should support accessors whose relative_xpath is a lookup array instead of an xpath string" do
-      AccessorTest.accessors[:title][:children][:language][:relative_xpath].should == [{:attribute=>"lang"}]
-      @sample.retrieve( [:title, 1] ).first.text.should == "Artikkelin otsikko Hydrangea artiklan 1"
-      @sample.retrieve( [:title, 1, :language] ).first.text.should == "finnish"
-    end
-    
-    it "should use Nokogiri to retrieve a NodeSet corresponding to the combination of accessor keys and array/nodeset indexes" do
-      mock_person = mock("person")
-      mock_persons_set = ["foo", mock_person]
-      mock_first_names_set = mock("first name nodeset")
-      
-      @sample.ng_xml.expects(:xpath).with('oxns:name[@type="person"]').returns(mock_persons_set)
-      mock_persons.expects(:xpath).with('namePart[@type="given"]').returns(mock_first_names_set)
-      
-      @sample.retrieve(:person, 1, :first_name).should == mock_first_names_set 
+      # pending "this only impacts scenarios where we want to display & edit"
+      AccessorTest.accessors[:title_info][:children][:language][:relative_xpath].should == {:attribute=>"lang"}
+      # @sample.retrieve( :title, 1 ).first.text.should == "Artikkelin otsikko Hydrangea artiklan 1"
+      @sample.retrieve( :title_info, 1, :language ).first.text.should == "finnish"
     end
     
   end
@@ -108,19 +110,29 @@ describe "OX::AccesorHelper" do
     end
   end
   
-  # describe "#accessor_xpath" do
-  #   it "should return the xpath given in the call to #accessor" do
-  #     AccessorTest.accessor_xpath( [:abstract] ).should == 'oxns:abstract'
-  #   end   
-  #   # Note: Ruby array indexes begin from 0.  In xpath queries (which start from 1 instead of 0), this will be translated accordingly.
-  #   it "should prepend the xpath for any parent nodes, inserting calls to xpath:position() function where necessary" do
-  #     AccessorTest.accessor_xpath( [:conference, 0, :role, 1, :text] ).should == 'oxns:name[@type="conference" and positon()=1]/oxns:role[position()=2]/roleTerm[@type="text"]'
-  #   end
-  #   it "should prepend the xpath for any parent nodes" do
-  #     AccessorTest.accessor_xpath( [:conference, conference_index, :text_role] ).should == 'oxns:name[@type="conference"]/oxns:role'
-  #   end
-  # end
-  # describe ".accessor_xpath" do
+  describe "#accessor_info" do
+    it "should return the xpath given in the call to #accessor" do
+      AccessorTest.accessor_info( :abstract ).should == AccessorTest.accessors[:abstract]
+    end
+    it "should return the xpath given in the call to #accessor" do
+      AccessorTest.accessor_info( :abstract ).should == AccessorTest.accessors[:abstract]
+    end
+    it "should dig into the accessors hash as far as you want, ignoring index values" do
+      AccessorTest.accessor_info( :conference, 0, :role, 1, :text ).should == AccessorTest.accessors[:conference][:children][:role][:children][:text]
+      AccessorTest.accessor_info( :conference, :role, :text ).should  == AccessorTest.accessors[:conference][:children][:role][:children][:text]
+    end
+  end
+    
+  describe "#accessor_xpath" do
+    it "should return the xpath given in the call to #accessor" do
+      AccessorTest.accessor_xpath( :abstract ).should == '//oxns:abstract'
+    end   
+    # Note: Ruby array indexes begin from 0.  In xpath queries (which start from 1 instead of 0), this will be translated accordingly.
+    it "should prepend the xpath for any parent nodes, inserting calls to xpath:position() function where necessary" do
+      AccessorTest.accessor_xpath( :conference, 0, :role, 1, :text ).should == '//oxns:name[@type="conference" and position()=1]/oxns:role[position()=2]/oxns:roleTerm[@type="text"]'
+    end
+  end
+  # describe ".accessor_xpath (instance method)" do
   #   it "should delegate to the class method" do
   #     AccessorTest.expects(:accessor_xpath).with( [:conference, conference_index, :text_role] )
   #     @sample.accessor_xpath( [:conference, conference_index, :role] )
