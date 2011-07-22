d491d65df49cea5daff11084f1a9503d666b695b
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-07-22 14:15:14 -0500

    coverage for an odd case with proxy terms

--- spec/unit/named_term_proxy_spec.rb
+++ spec/unit/named_term_proxy_spec.rb
@@ -5,21 +5,29 @@ describe "OM::XML::NamedTermProxy" do
   
   before(:all) do
     
-    @test_terminology_builder = OM::XML::Terminology::Builder.new do |t|
-      t.parent {
-        t.foo {
-          t.bar
+    class NamedProxyTestDocument 
+      include OM::XML::Document
+      set_terminology do |t|
+        t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd")
+        t.parent {
+          t.foo {
+            t.bar
+          }
+          t.my_proxy(:proxy=>[:foo, :bar])
         }
-        t.my_proxy(:proxy=>[:foo, :bar])
-      }
-      t.adoptive_parent(:ref=>[:parent], :attributes=>{:type=>"adoptive"})
-      t.parentfoobarproxy(:proxy=>[:parent, :foo, :bar])
+        t.adoptive_parent(:ref=>[:parent], :attributes=>{:type=>"adoptive"})
+        t.parentfoobarproxy(:proxy=>[:parent, :foo, :bar])
+      end
+      def self.xml_template 
+        '<mods xmlns="http://www.loc.gov/mods/v3"><parent><foo/></parent></mods>'
+      end
     end
     
-    @test_terminology = @test_terminology_builder.build
+    @test_terminology = NamedProxyTestDocument.terminology
     @test_proxy = @test_terminology.retrieve_term(:parent, :my_proxy)
     @proxied_term = @test_terminology.retrieve_term(:parent, :foo, :bar)
     @adoptive_parent = @test_terminology.retrieve_term(:adoptive_parent)
+
   end
     
   it "should proxy all extra methods to the proxied object" do
@@ -39,5 +47,10 @@ describe "OM::XML::NamedTermProxy" do
   end
   it "should support NamedTermProxies that point to root terms" do
     @test_terminology.xpath_for(:parentfoobarproxy).should == "//oxns:parent/oxns:foo/oxns:bar"
+  end
+  it "should be usable in update_values" do
+    document = NamedProxyTestDocument.from_xml(NamedProxyTestDocument.xml_template)
+    document.update_values([:parentfoobarproxy] => "FOObar!")
+    document.term_values(:parentfoobarproxy).should == ["FOObar!"]
   end
 end
