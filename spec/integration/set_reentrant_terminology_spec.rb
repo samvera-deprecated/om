05de691c204df6fc488a862b98ff0ddebaab0772
Chris Beer <chris@cbeer.info>
2012-05-22 11:16:17 -0700

    add #use_terminology and #extend_terminology methods to extend existing OM terminologies

--- spec/integration/set_reentrant_terminology_spec.rb
+++ spec/integration/set_reentrant_terminology_spec.rb
@@ -56,4 +56,79 @@ describe "calling set_terminology more than once" do
 
   end
 
+  describe "re-entrant modification" do
+
+
+    before(:all) do
+      class ReentrantTerminology
+        set_terminology do |t|
+          t.root :path => 'root', :xmlns => "asdf"
+          t.foo
+        end
+      end
+
+      class ReentrantTerminology
+        extend_terminology do |t|
+          t.bar
+        end
+      end
+    end
+
+    subject do
+      xml = '<root xmlns="asdf"><foo>fooval</foo><bar>barval</bar></root>'
+      ReentrantTerminology.from_xml(xml)
+    end
+
+    it "can get foo" do
+      subject.foo.should == ['fooval']
+    end
+
+    it "can get bar" do
+      subject.bar.should == ['barval']
+    end
+
+  end
+
+  describe "subclass modification" do
+
+
+    before(:all) do
+      class ReentrantTerminology
+        set_terminology do |t|
+          t.root :path => 'root', :xmlns => "asdf"
+          t.foo
+        end
+      end
+
+      class LocalReentrantTerminology 
+        include OM::XML::Document
+        use_terminology(ReentrantTerminology)
+        extend_terminology do |t|
+          t.bar
+        end
+      end
+    end
+
+    subject do
+      xml = '<root xmlns="asdf"><foo>fooval</foo><bar>barval</bar></root>'
+      LocalReentrantTerminology.from_xml(xml)
+    end
+
+    it "shouldn't bleed up the inheritence stack" do
+      xml = '<root xmlns="asdf"><foo>fooval</foo><bar>barval</bar></root>'
+      t = ReentrantTerminology.from_xml(xml)
+
+      expect { t.bar }.to raise_error OM::XML::Terminology::BadPointerError
+    end
+
+    it "can get foo" do
+      subject.foo.should == ['fooval']
+    end
+
+    it "can get bar" do
+      subject.bar.should == ['barval']
+    end
+
+  end
+
 end
