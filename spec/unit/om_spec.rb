cbe1f5876f04938c3492c48057cb595ff54be973
Joe Atzberger <ohiocore@gmail.com>
2015-07-22 19:34:43 -0700

    Remove weird useless interpolation braces
    This actually was breaking rubocop

--- spec/unit/om_spec.rb
+++ spec/unit/om_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "OM" do
-  
-  describe "#{}destringify" do
+
+  describe "#destringify" do
     it "should recursively change any strings beginning with : to symbols and any number strings to integers" do
       expect(OM.destringify( [{":person"=>"0"}, ":last_name"] )).to eq([{:person=>0}, :last_name])
       expect(OM.destringify( [{"person"=>"3"}, "last_name"] )).to eq([{:person=>3}, :last_name])
     end
   end
-  
+
 end
